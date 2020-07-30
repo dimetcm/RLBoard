58cbc9371087b38116b98b77e39a924fa77c6ed2
dimetcm <dimetcm@gmail.com>
2020-07-30 23:38:36 +0200

    adding Move type

--- code/go/gotypes.py
+++ code/go/gotypes.py
@@ -22,3 +22,42 @@ class Point(namedtuple('Point', 'row col')):
     def __deepcopy__(self, memodict={}):
         # These are very immutable.
         return self
+
+class Move():
+    """Any action a player can play on a turn.
+
+    Exactly one of is_play, is_pass, is_resign will be set.
+    """
+    def __init__(self, point=None, is_pass=False, is_resign=False):
+        assert (point is not None) ^ is_pass ^ is_resign
+        self.point = point
+        self.is_play = (self.point is not None)
+        self.is_pass = is_pass
+        self.is_resign = is_resign
+
+    @classmethod
+    def play(cls, point):
+        """A move that places a stone on the board."""
+        return Move(point=point)
+
+    @classmethod
+    def pass_turn(cls):
+        return Move(is_pass=True)
+
+    @classmethod
+    def resign(cls):
+        return Move(is_resign=True)
+
+    def __str__(self):
+        if self.is_pass:
+            return 'pass'
+        if self.is_resign:
+            return 'resign'
+        return '(r %d, c %d)' % (self.point.row, self.point.col)
+
+    def __hash__(self):
+        return hash((self.is_play, self.is_pass, self.is_resign, self.point))
+
+    def __eq__(self, other):
+        return (self.is_play, self.is_pass, self.is_resign, self.point) ==\
+               (other.is_play, other.is_pass, other.is_resign, other.point)
