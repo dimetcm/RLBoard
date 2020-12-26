2256c412d5a6fba58309c1b136eadc7c5a54dfc1
dimetcm <dimetcm@gmail.com>
2020-12-26 18:54:57 +0100

    chess board gui

--- code/chess/chessgui.py
+++ code/chess/chessgui.py
@@ -1,44 +1,100 @@
 import tkinter as tk
+from PIL import Image, ImageTk
 
-board_size = 300
-board_square_size = board_size / 8.0
-
-
-def convert_position_to_square_name(x, y):
-    # todo: support board flipping
-    files_str = "ABCDEFGH"
-    file_idx = x // int(board_square_size)
-    file_idx = min(file_idx, 7)
-
-    rank = (board_size - y) // int(board_square_size)
-    rank = min(rank, 7)
-    return files_str[file_idx] + str(rank + 1)
+FILES_STR = "ABCDEFGH"
 
 
-def move(event):
-    print(convert_position_to_square_name(event.x, event.y))
+class Square:
+
+    def __init__(self, square_name):
+        assert len(square_name) == 2
+        assert square_name[0] in FILES_STR
+        assert 1 <= int(square_name[1]) <= 8
+
+        self.file_idx = FILES_STR.index(square_name[0])
+        self.rank_idx = int(square_name[1]) - 1
 
 
-def create_board():
-    frame = tk.Frame()
-    frame.pack()
+class ChessBoardGUI:
 
-    board_canvas = tk.Canvas(frame, bg="black", height=board_size + 1, width=board_size + 1, borderwidth=0,
-                             highlightthickness=0)
-    board_canvas.pack()
-
-    for y in range(8):
-        for x in range(8):
-            x_coord = x * board_square_size
-            y_coord = y * board_square_size
-
-            color = "wheat" if (x + y) % 2 == 0 else "black"
-            board_canvas.create_rectangle(x_coord, y_coord, x_coord + board_square_size, y_coord + board_square_size,
-                                          outline="black", fill=color)
-
-    board_canvas.bind('<Motion>', move)
+    def __init__(self, board_size = 400):
+        self.board_size = board_size
+        self.board_square_size = board_size / 8.0
+        self.images = {}
+        self.board_canvas = None
+        self.window = tk.Tk()
+        self.__create_board()
+        self.__load_images()
 
 
-window = tk.Tk()
-create_board()
-window.mainloop()
+    def __create_board(self):
+        frame = tk.Frame()
+        frame.pack()
+
+        self.board_canvas = tk.Canvas(frame, bg="black", height=self.board_size + 1, width=self.board_size + 1, borderwidth=0,
+                                 highlightthickness=0)
+        self.board_canvas.pack()
+
+        for y in range(8):
+            for x in range(8):
+                x_coord = x * self.board_square_size
+                y_coord = y * self.board_square_size
+
+                color = "wheat" if (x + y) % 2 == 0 else "sienna"
+                self.board_canvas.create_rectangle(x_coord, y_coord, x_coord + self.board_square_size,
+                                              y_coord + self.board_square_size,
+                                              outline="black", fill=color)
+
+        self.board_canvas.bind('<Motion>', self.__move)
+
+    def __load_images(self):
+        # lowercase for black, uppercase for white
+        filenames = {
+            'p': "b_pawn_png_512px.png",
+            'r': "b_rook_png_512px.png",
+            'n': "b_knight_png_512px.png",
+            'b': "b_bishop_png_512px.png",
+            'q': "b_queen_png_512px.png",
+            'k': "b_king_png_512px.png",
+            'P': "w_pawn_png_512px.png",
+            'N': "w_knight_png_512px.png",
+            'R': "w_rook_png_512px.png",
+            'B': "w_bishop_png_512px.png",
+            'Q': "w_queen_png_512px.png",
+            'K': "w_king_png_512px.png",
+        }
+
+        path = "../../data/chess pieces/512h/"
+        for k, v in filenames.items():
+            # print(path + v)
+            image_file = Image.open(path + v)
+            image_file = image_file.resize((int(self.board_square_size - 5), int(self.board_square_size - 5)), Image.ANTIALIAS)
+            self.images[k] = ImageTk.PhotoImage(image_file)
+
+    def __move(self, event):
+        print(self.convert_position_to_square_name(event.x, event.y))
+
+    def convert_position_to_square_name(self, x, y):
+        # todo: support board flipping
+        file_idx = x // int(self.board_square_size)
+        file_idx = min(file_idx, 7)
+
+        rank = (self.board_size - y) // int(self.board_square_size)
+        rank = min(rank, 7)
+        return FILES_STR[file_idx] + str(rank + 1)
+
+    def show(self):
+        self.window.mainloop()
+
+    def add_piece(self, piece, square):
+        assert piece in self.images
+
+        x_pos = (square.file_idx + 0.5) * self.board_square_size
+        y_pos = (7 - square.rank_idx + 0.5) * self.board_square_size
+        self.board_canvas.create_image(x_pos, y_pos, image=self.images[piece])
+
+
+boardGUI = ChessBoardGUI()
+boardGUI.add_piece('p', Square('A7'))
+boardGUI.add_piece('k', Square('E8'))
+boardGUI.show()
